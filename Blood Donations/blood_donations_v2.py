78f67637e323a65fab9ac789eff63bdee7d48e23
iholas <igor.holas@gmail.com>
2016-10-17 11:23:26 -0500

    better comment atop donations v2

--- Blood Donations/blood_donations_v2.py
+++ Blood Donations/blood_donations_v2.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 10 16:11:11 2016
-
 @author: igor
 
-Implementing three classification methods to predict blood donation
-1) simple lgistic regression
-2) Stochaistic Gradient Descent Classifier
-3) ADA Boosted Decision Tree Classifier 
+Implementing a cross-validated classifier solution. 
 
-Currently the SGDC method is very lousy, must try to improve
-Logistic regression has decent accuracy. but poor recall
-ADABoost Classifier has good Accuracy, but still sub 0.5 recall.  
+Included models are:  
+* Logistic regression  
+* Linear discriminant analysis  
+* K neighbors classifier  
+* Decision tree classifier  
+* Naive Bayes  
+* SVMclassifier  
+
+KNN performs the best with ~0.72 precision and ~0.45 recall on predicting donations
 """
 # Load libraries
 import os
